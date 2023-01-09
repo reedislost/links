709d28d4c7f0624550e29c9eb750adb51724771e
reedislost <reed@lostfr8.com>
2023-01-08 20:17:53 -0700

    third

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
-import { Center, Box, VStack, Link, Text } from "@chakra-ui/react";
+import {
+  Center,
+  Box,
+  VStack,
+  Link,
+  Text,
+  HStack,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,14 +23,29 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Center minH={"100vh"} flexDirection={"column"}>
+      <Center minH={"85vh"} flexDirection={"column"}>
         <Box>
-          <Text fontSize={"5xl"}>Links</Text>
+          <Text fontSize={useBreakpointValue({ base: "5xl", md: "7xl" })}>
+            🔗 LINKS 🔗
+          </Text>
         </Box>
-        <VStack mt={9} fontSize="2xl">
-          <Link href="https://basecamp.com/gettingreal">getting real</Link>
-          <Link href="https://buildspace.so/builds">buildspace</Link>
+        <VStack
+          mt={9}
+          fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
+        >
+          <Link href="https://basecamp.com/gettingreal">getting real 📕</Link>
+          <Link href="https://lostfr8">LOSTFR8 🚚</Link>
+          <Link href="https://www.reedislost.com/">reedislost.com 💻</Link>
         </VStack>
+      </Center>
+      <Center>
+        <HStack
+          fontSize={useBreakpointValue({ base: "3xl", md: "5xl" })}
+          spacing={5}
+        >
+          <Link href="https://twitter.com/lostisreed"> 🐦</Link>
+          <Link href="mailto:reed@lostfr8.com"> 📪</Link>
+        </HStack>
       </Center>
     </>
   );
